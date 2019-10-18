c538a65cf4ce09508ebe8f482d59cf765ce18bcb
nadyahruth <36093826+nadyahruth@users.noreply.github.com>
2019-10-18 17:44:11 -0400

    HW 2 Design Dont Code Eh
    Trying to fix the load function for the listview to load the data properly.  Most of the code so far is taken from List View Sample

---
+++ HW2/HW2/frmMain.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.munOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuSave = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuStatistics = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.ofdOpen = New System.Windows.Forms.OpenFileDialog()
+        Me.sfdSave = New System.Windows.Forms.SaveFileDialog()
+        Me.lvwTaxData = New System.Windows.Forms.ListView()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.ViewToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1626, 33)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.munOpen, Me.mnuSave, Me.mnuExit})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(54, 29)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'munOpen
+        '
+        Me.munOpen.Name = "munOpen"
+        Me.munOpen.Size = New System.Drawing.Size(270, 34)
+        Me.munOpen.Text = "Open"
+        '
+        'mnuSave
+        '
+        Me.mnuSave.Name = "mnuSave"
+        Me.mnuSave.Size = New System.Drawing.Size(270, 34)
+        Me.mnuSave.Text = "Save"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(270, 34)
+        Me.mnuExit.Text = "Exit"
+        '
+        'ViewToolStripMenuItem
+        '
+        Me.ViewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuStatistics})
+        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(65, 29)
+        Me.ViewToolStripMenuItem.Text = "View"
+        '
+        'mnuStatistics
+        '
+        Me.mnuStatistics.Name = "mnuStatistics"
+        Me.mnuStatistics.Size = New System.Drawing.Size(270, 34)
+        Me.mnuStatistics.Text = "Statistics"
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(891, 573)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(118, 51)
+        Me.btnLoad.TabIndex = 2
+        Me.btnLoad.Text = "Load"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(1432, 573)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(118, 51)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(1251, 573)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(118, 51)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(1068, 573)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(118, 51)
+        Me.btnSave.TabIndex = 5
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'ofdOpen
+        '
+        Me.ofdOpen.FileName = "OpenFileDialog1"
+        '
+        'lvwTaxData
+        '
+        Me.lvwTaxData.HideSelection = False
+        Me.lvwTaxData.Location = New System.Drawing.Point(35, 57)
+        Me.lvwTaxData.Name = "lvwTaxData"
+        Me.lvwTaxData.Size = New System.Drawing.Size(1547, 480)
+        Me.lvwTaxData.TabIndex = 6
+        Me.lvwTaxData.UseCompatibleStateImageBehavior = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1626, 637)
+        Me.Controls.Add(Me.lvwTaxData)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmMain"
+        Me.Text = "WestEnd Tax Data"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents munOpen As ToolStripMenuItem
+    Friend WithEvents mnuSave As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents mnuStatistics As ToolStripMenuItem
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents ofdOpen As OpenFileDialog
+    Friend WithEvents sfdSave As SaveFileDialog
+    Friend WithEvents lvwTaxData As ListView
+End Class
---
+++ HW2/HW2/frmMain.vb
@@ -0,0 +1,161 @@
+﻿Imports System.IO
+Public Class frmMain
+    Private strFileName As String
+    Private dblTotalInvValue As Double
+    Private intTotalCount As Integer
+    Private arrCategories As ArrayList
+    'Private Stats As frmStats
+#Region "Column Contstants"
+    'contansts to maanage the listView columns
+    Private Const EMP_ID As Integer = 0
+    Private Const LAST_NAME As Integer = 1
+    Private Const FIRST_NAME As Integer = 2
+    Private Const JOB_CODE As Integer = 3
+    Private Const DEPTARTMENT As Integer = 4
+    Private Const EVALUATION As Integer = 5
+    Private Const RATING As Integer = 6
+    Private Const BASE_SALARY As Integer = 7
+    Private Const BONUS_PERCENT As Integer = 8
+    Private Const BONUS_AMT As Integer = 9
+    Private Const TOTAL_PAID As Integer = 10
+#End Region
+    Private Sub OpenFile()
+        Dim intResult As Integer
+        ofdOpen.InitialDirectory = Application.StartupPath
+        ofdOpen.Filter = "All files(*.*) |*.*| Text files (*.txt)| *.txt"
+        ofdOpen.FilterIndex = 2
+        intResult = ofdOpen.ShowDialog
+        If intResult = DialogResult.Cancel Then 'user cancelled the open
+            Exit Sub
+        End If
+        strFileName = ofdOpen.FileName
+        Try
+            ReadinputFile(strFileName)
+        Catch exNotFound As FileNotFoundException
+            'put error handling code here
+            'message box for example then exit
+        Catch exIOError As IOException
+            'put error handling code here
+
+        Catch ex As Exception 'anything else that might go wrong
+            'put error handling code here
+        End Try
+    End Sub
+    Private Sub SaveFile()
+        Dim intResult As Integer
+        sfdSave.InitialDirectory = Application.StartupPath
+        sfdSave.Filter = "All files (*.*)| *.*| Text files (*.txt)|*.txt"
+        sfdSave.FilterIndex = 2
+        intResult = sfdSave.ShowDialog
+        If intResult = DialogResult.Cancel Then
+            Exit Sub
+        End If
+        strFileName = sfdSave.FileName
+        Try
+            writeOutputFile(strFileName)
+        Catch exNotFound As FileNotFoundException
+            'put error handling code here
+        Catch exIoError As IOException
+
+            'put error handling code here
+        Catch exOther As Exception ' anything else that might go wrong 
+            'put error handling code here
+        End Try
+    End Sub
+    Private Sub writeOutputFile(strName As String)
+        Dim fileOut As StreamWriter
+        Dim strLineOut As String = ""
+        Dim i As Integer
+        Dim j As Integer
+        Try
+            fileOut = New StreamWriter(strName)
+            'build the field names as the first line in the output file by reading the column names
+            For i = 0 To lvwTaxData.Columns.Count - 1
+                If i < lvwTaxData.Columns.Count - 1 Then ' not the last column
+                    strLineOut &= lvwTaxData.Columns(i).Text
+                End If
+            Next
+            'write out the heading to the output file 
+            fileOut.WriteLine(strLineOut)
+            'build each data line with commas separtating teh feils 
+            'by looping through the rows adn columns of the listview
+            For i = 0 To lvwTaxData.Items.Count - 1
+                strLineOut = lvwTaxData.Items(i).Text ' Write the first dulmn items
+                For j = 1 To lvwTaxData.Items(i).SubItems.Count - 1
+                    strLineOut &= "," & lvwTaxData.Items(i).SubItems(j).Text
+                Next
+                fileOut.WriteLine(strLineOut)
+            Next
+            fileOut.Close()
+            fileOut.Dispose()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+    Private Sub ReadInputFile(strIn As String)
+        Dim fileIn As StreamReader
+        Dim strLineIn As String
+        Dim strFields() As String 'string array to hold the field brom a record in the file
+        Dim i As Integer
+        lvwTaxData.Items.Clear()
+        Try
+            fileIn = New StreamReader(strIn)
+            If Not fileIn.EndOfStream Then 'get the first line adn use it to build the column headings
+                strLineIn = fileIn.ReadLine 'will read each line 
+                strFields = strLineIn.Split(",") ' comma is the delimiter and what is used to spearte each value in the line
+                For i = 0 To strFields.Length - 1 'build column headings
+                    lvwTaxData.Columns.Add(strFields(i)) 'anthing that is ith will become headings in the listview
+                Next
+
+                ' do some formatting of the columns 
+                With lvwTaxData ' with sets a temp naming concept(shortcut)
+                    .Columns(EMP_ID).Width = 80
+                    .Columns(LAST_NAME).Width = 100
+                    .Columns(FIRST_NAME).Width = 100
+                    .Columns(JOB_CODE).Width = 50
+                    .Columns(DEPTARTMENT).Width = 30
+                    .Columns(EVALUATION).Width = 80
+                    .Columns(RATING).Width = 80
+                    .Columns(BASE_SALARY).Width = 150
+                    .Columns(BONUS_PERCENT).Width = 80
+                    .Columns(BONUS_AMT).Width = 150
+
+                    .Columns(TOTAL_PAID).TextAlign = HorizontalAlignment.Right
+
+                End With
+            End If
+            'now get data for each row
+            While Not fileIn.EndOfStream
+                strLineIn = fileIn.ReadLine
+                strFields = strLineIn.Split(",")
+                'set up the first column value in a new listview item (the row)
+                Dim lviRow As New ListViewItem(strFields(0))
+                'now add the rest of the column values as subitems to that listview item 
+                For i = 1 To strFields.Length - 1
+                    Dim lsiCal As New ListViewItem.ListViewSubItem
+                    If i <> BASE_SALARY Or BONUS_AMT Or TOTAL_PAID Then
+                        lsiCal.Text = strFields(i)
+                    Else ' it is the money value so format it 
+                        lsiCal.Text = FormatCurrency(strFields(i), 0)
+                    End If
+                    lviRow.SubItems.Add(lsiCal) ' add column to the row 
+                Next
+                'now add the completed row to the listview
+                lvwTaxData.Items.Add(lviRow)
+                'UpdateStatistics(lviRow)
+            End While
+            fileIn.Close()
+            fileIn.Dispose()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        OpenFile()
+    End Sub
+
+    Private Sub munOpen_Click(sender As Object, e As EventArgs) Handles munOpen.Click
+        OpenFile()
+    End Sub
+End Class
